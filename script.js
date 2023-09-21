e27a4ddfee2910936568214ce4e92cf61b4def81
aradhya0507 <145675303+aradhya0507@users.noreply.github.com>
2023-09-21 17:31:34 +0530

    Add files via upload
    File Upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var list = document.querySelectorAll('img')
+
+const first = Math.floor(Math.random() * 6 ) + 1;
+const imagefirst = 'assets/dice'+first+'.png'
+list[0].setAttribute('src', imagefirst);
+
+const second = Math.floor(Math.random() * 6 ) + 1;
+const imagesecond = 'assets/dice'+second+'.png'
+list[1].setAttribute('src', imagesecond);
+
+if( first > second){
+    document.querySelector('h1').innerHTML = "The winner is user 1" ; 
+}
+else if(first < second){
+    document.querySelector('h1').innerHTML = "The winner is user 2"  ;  
+}
+else {
+    document.querySelector('h1').innerHTML = "It's a tie!";
+}
